c17f873c241ab36e319c48a8e1ca39fab2fc275e
mayaswan <33310757+mayaswan@users.noreply.github.com>
2019-08-06 11:35:32 +0700

    Update pagarhosts_V2_testItems.py

--- pagarhosts_V2_testItems.py
+++ pagarhosts_V2_testItems.py
@@ -1,21 +1,52 @@
-#!/usr/bin/sh
-import reqeusts
-import json 
-resp=reqeusts.post('http://192.168.120.104/zabbix/api_jsonrpc.php')
-HEADER='Content-Type:application/json'
+# vim: set fileencoding=utf8
+#
+# authenticate with zabbix api server, and retrieve monitored hosts
+# specification : https://www.zabbix.com/documentation/1.8/api
+#
+# $ python zabbix_api.py
+import requests
+from pprint import pprint
+import json
 
-USER = "Admin"
-PASS = "zabbix"
+ZABIX_ROOT = 'http://localhost/zabbix'
+url = ZABIX_ROOT + '/api_jsonrpc.php'
 
-r = resp
-
-r = {
-    "jsonrpc": "2.0",
-    "method": "user.login",
+########################################
+# user.login
+########################################
+payload = {
+    "jsonrpc" : "2.0",
+    "method" : "user.login",
     "params": {
-        "user": 'USER',
-        "password": 'PASS'
+      'user': 'api',
+      'password':'api',
     },
-    "id": 1
+    "auth" : None,
+    "id" : 0,
 }
+headers = {
+    'content-type': 'application/json',
+}
+res  = requests.post(url, data=json.dumps(payload), headers=headers)
+res = res.json()
+print 'user.login response'
+pprint(res)
 
+########################################
+# host.get
+########################################
+payload = {
+    "jsonrpc" : "2.0",
+    "method" : "host.get",
+    "params": {
+      'output': [
+          'hostid',
+          'name'],
+    },
+    "auth" : res['result'],
+    "id" : 2,
+}
+res2 = requests.post(url, data=json.dumps(payload), headers=headers)
+res2 = res2.json()
+print 'host.get response'
+pprint(res2)
