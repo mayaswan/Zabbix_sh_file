9b48168023c6d285a4a3f938eafc868baebebc02
mayaswan <33310757+mayaswan@users.noreply.github.com>
2019-08-06 10:52:09 +0700

    Update pagarhosts_V2_testItems.py

--- pagarhosts_V2_testItems.py
+++ pagarhosts_V2_testItems.py
@@ -1,13 +1,15 @@
-#!/bin/sh
+#!/usr/bin/sh
 import reqeusts
-
+import json 
 resp=reqeusts.post('http://192.168.120.104/zabbix/api_jsonrpc.php')
 HEADER='Content-Type:application/json'
 
 USER = "Admin"
 PASS = "zabbix"
 
-Login(){
+r = resp
+
+r = {
     "jsonrpc": "2.0",
     "method": "user.login",
     "params": {
