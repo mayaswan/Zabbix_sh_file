fbea77e0aa16ee6d91f7da05eeb414a94b151388
mayaswan <33310757+mayaswan@users.noreply.github.com>
2019-08-06 17:57:51 +0700

    Update pagarhosts_V2_testItems.py

--- pagarhosts_V2_testItems.py
+++ pagarhosts_V2_testItems.py
@@ -11,64 +11,64 @@ import json
 
 ZABIX_ROOT = 'http://192.168.120.104/zabbix'
 url = ZABIX_ROOT + '/api_jsonrpc.php'
-
+while True:
 ########################################
 # user.login
 ########################################
-payload = {
-    "jsonrpc" : "2.0",
-    "method" : "user.login",
-    "params": {
-      'user': "Admin",
-      'password':"zabbix",
-    },
-    "auth" : None,
-    "id" : 0,
-}
-headers = {
-    'content-type': 'application/json',
-}
-res  = requests.post(url, data=json.dumps(payload), headers=headers)
-res = res.json()
-#print 'user.login response'
-pprint(res)
+        payload = {
+            "jsonrpc" : "2.0",
+            "method" : "user.login",
+            "params": {
+              'user': "Admin",
+              'password':"zabbix",
+            },
+            "auth" : None,
+            "id" : 0,
+        }
+        headers = {
+            'content-type': 'application/json',
+        }
+        res  = requests.post(url, data=json.dumps(payload), headers=headers)
+        res = res.json()
+        #print 'user.login response'
+        pprint(res)
 
-########################################
-# host.get
-########################################
-payload = {
-    "jsonrpc" : "2.0",
-    "method" : "host.get",
-    "params": {
-      'output': [
-          'hostid',
-          'name'],
-    },
-    "auth" : res['result'],
-    "id" : 1,
-}
-res2 = requests.post(url, data=json.dumps(payload), headers=headers)
-res2 = res2.json()
-#print 'host.get response'
-pprint(res2)
-########################################
-# item.get
-########################################
-payload = {
-    "jsonrpc": "2.0",
-    "method": "item.get",
-    "params": {
-        "output": "extend",
-        "hostids": "10299",
-        "search": {
-            "key_1": ""
-        },
-        "sortfield": "name"
-    },
-    "auth": res['result'],
-    "id": 2
-}
-res3 = requests.post(url, data=json.dumps(payload), headers=headers)
-res3 = res3.json()
-#print 'host.get response'
-pprint(res3)
+        ########################################
+        # host.get
+        ########################################
+        payload = {
+            "jsonrpc" : "2.0",
+            "method" : "host.get",
+            "params": {
+              'output': [
+                  'hostid',
+                  'name'],
+            },
+            "auth" : res['result'],
+            "id" : 1,
+        }
+        res2 = requests.post(url, data=json.dumps(payload), headers=headers)
+        res2 = res2.json()
+        #print 'host.get response'
+        pprint(res2)
+        ########################################
+        # item.get
+        ########################################
+        payload = {
+            "jsonrpc": "2.0",
+            "method": "item.get",
+            "params": {
+                "output": "extend",
+                "hostids": "10299",
+                "search": {
+                    "key_1": ""
+                },
+                "sortfield": "name"
+            },
+            "auth": res['result'],
+            "id": 2
+        }
+        res3 = requests.post(url, data=json.dumps(payload), headers=headers)
+        res3 = res3.json()
+        #print 'host.get response'
+        pprint(res3)
